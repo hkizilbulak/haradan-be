eee4dc3c8b7aa703ae891914a285bbee3ec930c4
Sami Yusuf Avcı <samiyusufavci77@gmail.com>
2026-09-18 20:48:50 +0300

    chore: update scratch script to test admin user deletion logic

--- scratch.go
+++ scratch.go
@@ -6,25 +6,42 @@ import (
 	"os"
 
 	"github.com/google/uuid"
-	"github.com/joho/godotenv"
 	"github.com/jackc/pgx/v5/pgxpool"
-	pgnotif "github.com/hkizilbulak/haradan-be/internal/infrastructure/postgres/notification"
+	"github.com/joho/godotenv"
+
+	pgadmin "github.com/hkizilbulak/haradan-be/internal/infrastructure/postgres/adminuser"
 )
 
 func main() {
 	godotenv.Load(".env")
-	godotenv.Load(".env.local")
 	dbURL := os.Getenv("DATABASE_URL")
-	pool, _ := pgxpool.New(context.Background(), dbURL)
-	defer pool.Close()
-	
-	repo := pgnotif.NewRepository(pool)
-	
-	u, _ := uuid.Parse("c7c58902-f99a-41aa-8725-c6a4c015fbf9")
-	res, err := repo.ListUserNotifications(context.Background(), u, nil, nil, 20)
-	fmt.Println("Result length:", len(res))
-	if len(res) > 0 {
-		fmt.Printf("Item: %+v\n", res[0])
+	pool, err := pgxpool.New(context.Background(), dbURL)
+	if err != nil {
+		panic(err)
 	}
-	fmt.Println("Error:", err)
+	defer pool.Close()
+
+	ctx := context.Background()
+
+	targetUserID := uuid.MustParse("6e7702c7-ae9b-4564-9d17-68fb6326f76e")
+	var adminID uuid.UUID
+	err = pool.QueryRow(ctx, "SELECT id FROM hrd_users WHERE role = 'admin' LIMIT 1").Scan(&adminID)
+	if err != nil {
+		fmt.Println("Find admin error:", err)
+		return
+	}
+	fmt.Println("Target user ID:", targetUserID)
+	fmt.Println("Admin ID:", adminID)
+
+	repo := pgadmin.NewRepository(pool)
+	tx, err := pool.Begin(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer tx.Rollback(ctx)
+
+	txRepo := repo.WithTx(tx)
+	err = txRepo.DeleteUser(ctx, targetUserID, adminID)
+	fmt.Printf("DeleteUser error: %+v\n", err)
 }
+
